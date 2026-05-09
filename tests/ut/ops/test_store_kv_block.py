94ef7c603e722c51865e4d024f3e58ba83bacac7
Shy <Sakura.iostream@gmail.com>
2026-05-09 08:59:21 +0000

    [Mod] Modified ut file of store_kv_block
    Signed-off-by: Shy <Sakura.iostream@gmail.com>

--- tests/ut/ops/test_store_kv_block.py
+++ tests/ut/ops/test_store_kv_block.py
@@ -32,7 +32,7 @@ def assert_store_kv_block_registered():
         "store_kv_block",
     ), "torch.ops._C_ascend.store_kv_block is not registered"
     
-def golden_store_kv_cache(keylist, cache_table, slotmap, block_size):
+def golden_store_kv_block(keylist, cache_table, slotmap, block_size):
     expected_cache = cache_table.clone()
 
     for token_idx, slot in enumerate(slotmap):
@@ -52,7 +52,7 @@ def golden_store_kv_cache(keylist, cache_table, slotmap, block_size):
 @pytest.mark.parametrize("block_size", [128])
 @pytest.mark.parametrize("num_blocks", [1773])
 @pytest.mark.parametrize("head_size", [128])
-def test_storeKvCacheS_with_continuous(
+def test_storeKVBlock_with_continuous(
     num_tokens,
     num_head,
     num_blocks,
@@ -90,7 +90,7 @@ def test_storeKvCacheS_with_continuous(
         dtype=torch.float16,
     )
 
-    expected_cache = golden_store_kv_cache(
+    expected_cache = golden_store_kv_block(
         keylist=keylist,
         cache_table=cache_table,
         slotmap=slotmap,
@@ -143,7 +143,7 @@ def test_storeKvCacheS_with_continuous(
 @pytest.mark.parametrize("block_size", [128])
 @pytest.mark.parametrize("num_blocks", [1773])
 @pytest.mark.parametrize("head_size", [512])
-def test_storeKvCacheS_without_continuous(
+def test_storeKVBlock_without_continuous(
     num_tokens,
     num_head,
     num_blocks,
@@ -182,7 +182,7 @@ def test_storeKvCacheS_without_continuous(
         size=(num_blocks, block_size, num_head, head_size),
         dtype=torch.float16,
     )
-    # expected_cache = golden_store_kv_cache(
+    # expected_cache = golden_store_kv_block(
     #     keylist=keylist,
     #     cache_table=cache_table,
     #     slotmap=slotmap,
@@ -238,7 +238,7 @@ def test_storeKvCacheS_without_continuous(
 @pytest.mark.parametrize("block_size", [128])
 @pytest.mark.parametrize("num_blocks", [1773])
 @pytest.mark.parametrize("head_size", [64])
-def test_storeKvCacheS_without_ascending(
+def test_storeKVBlock_without_ascending(
     num_tokens,
     num_head,
     num_blocks,
@@ -268,7 +268,7 @@ def test_storeKvCacheS_without_ascending(
         dtype=torch.int8,
     )
 
-    expected_cache = golden_store_kv_cache(
+    expected_cache = golden_store_kv_block(
         keylist=keylist,
         cache_table=cache_table,
         slotmap=slotmap,
@@ -323,7 +323,7 @@ def test_storeKvCacheS_without_ascending(
 @pytest.mark.parametrize('block_size', [128])#128
 @pytest.mark.parametrize('num_blocks', [1773])#1599
 @pytest.mark.parametrize('head_size', [512])
-def test_siso_with_continuous(num_tokens, num_head, block_size, num_blocks, head_size):
+def test_siso(num_tokens, num_head, block_size, num_blocks, head_size):
     key_cpu = torch.randint(
         low=0,
         high=128,
@@ -353,7 +353,7 @@ def test_siso_with_continuous(num_tokens, num_head, block_size, num_blocks, head
     slot_list_np = np.array(slot_list)
     slot_mapping_npu = torch.from_numpy(slot_list_np).to(torch.int32).npu()
 
-    key_expect = golden_store_kv_cache(
+    key_expect = golden_store_kv_block(
         keylist=key_cpu,
         cache_table=key_cache_cpu,
         slotmap=slot_list,
@@ -377,163 +377,3 @@ def test_siso_with_continuous(num_tokens, num_head, block_size, num_blocks, head
     # torch.ops._C_ascend.reshape_and_cache(key, value, key_cache, value_cache, slot_mapping)
     torch.testing.assert_close(key_expect, key_cache.cpu(), atol=0, rtol=0)
     # torch.testing.assert_close(key_expect, key_cache, atol=0.001, rtol=0.1 )
-   
-@pytest.mark.parametrize("num_tokens", [32*1024])
-@pytest.mark.parametrize("num_head", [1])
-@pytest.mark.parametrize("block_size", [128])
-@pytest.mark.parametrize("num_blocks", [1773])
-@pytest.mark.parametrize("head_size", [64])
-def test_siso_without_continuous(num_tokens, num_head, block_size, num_blocks,head_size):
-    key = torch.rand((num_tokens, num_head, head_size), dtype=torch.float16).npu()
-    key_cache = torch.rand((num_blocks, block_size, num_head, head_size), dtype=torch.float16).npu()
-    slot_list=[]
-    r = 0
-    for i in range(0, num_tokens):
-        r = r + random_with_zero_prob(0.85,5)
-        slot_list.append(i+r)
-    
-    assert num_tokens==len(slot_list)
-    slot_list_np= np.array(slot_list)
-    slot_mapping_npu = torch.from_numpy(slot_list_np).to(torch.int32).npu()
-    key_expect = golden_store_kv_cache(key, key_cache, slot_mapping_npu,block_size)
-    epoch = 100
-    start = time.perf_counter()
-    for _ in range(epoch):
-        torch_npu._npu_reshape_and_cache_siso(key, key_cache, slot_mapping_npu)
-    end = time.perf_counter()
-    avg_ms = (end - start) / epoch * 1000
-    print(f"python 耗时: {avg_ms:.4f} ms")
-    # prof.stop()
-    # end = time.perf_counter()
-    # avg_ms = (end - start) / N * 1000
-    # print(f"python 耗时: {avg_ms:.4f} ms")
-    # torch.ops._C_ascend.reshape_and_cache(key, value, key_cache, value_cache, slot_mapping)
-    torch.testing.assert_close(key_expect, key_cache, atol=0.001, rtol=0.1 )
-        
-# @pytest.mark.parametrize('num_tokens', [32*1024])#6398
-# @pytest.mark.parametrize('num_head', [1])#512
-# @pytest.mark.parametrize('block_size', [128])#128
-# @pytest.mark.parametrize('num_blocks', [1773])#1599
-# @pytest.mark.parametrize('count', [1])
-# def test_myops(num_tokens, num_head, block_size, num_blocks,count):
-#     print("PYTEST ASCEND_CUSTOM_OPP_PATH =", os.environ.get("ASCEND_CUSTOM_OPP_PATH"), flush=True)
-#     print("PYTEST ASCEND_OPP_PATH =", os.environ.get("ASCEND_OPP_PATH"), flush=True)
-#     print("PYTEST LD_LIBRARY_PATH has custom opapi:",
-#         "vllm_ascend/_cann_ops_custom/vendors/vllm-ascend/op_api/lib"
-#         in os.environ.get("LD_LIBRARY_PATH", ""),
-#         flush=True)
-
-#     # experimental_config = torch_npu.profiler._ExperimentalConfig(
-#     #     export_type=[
-#     #         torch_npu.profiler.ExportType.Text,
-#     #         torch_npu.profiler.ExportType.Db
-#     #         ],
-#     #     profiler_level=torch_npu.profiler.ProfilerLevel.Level0,
-#     #     msprof_tx=False,
-#     #     aic_metrics=torch_npu.profiler.AiCMetrics.AiCoreNone,
-#     #     l2_cache=False,
-#     #     op_attr=False,
-#     #     data_simplification=False,
-#     #     record_op_args=False,
-#     #     gc_detect_threshold=None,
-#     #     host_sys=[
-#     #         torch_npu.profiler.HostSystem.CPU,
-#     #         torch_npu.profiler.HostSystem.MEM],
-#     #     sys_io=False,
-#     #     sys_interconnection=False
-#     # )
-
-#     # prof = torch_npu.profiler.profile(
-#     #     activities=[
-#     #         torch_npu.profiler.ProfilerActivity.CPU,
-#     #         torch_npu.profiler.ProfilerActivity.NPU
-#     #         ],
-#     #     schedule=torch_npu.profiler.schedule(wait=0, warmup=0, active=1, repeat=1, skip_first=0),
-#     #     on_trace_ready=torch_npu.profiler.tensorboard_trace_handler("/home/z00893411/ops/script/ls"),
-#     #     record_shapes=True,
-#     #     profile_memory=False,
-#     #     with_stack=True,
-#     #     with_modules=False,
-#     #     with_flops=False,
-#     #     experimental_config=experimental_config)
-
-#     head_size =64
-#     # key_cache = torch.rand((num_blocks, block_size, num_head,head_size), dtype=torch.float16)
-#     key_cache = torch.randint(low=0,high=128,size=(num_blocks, block_size, num_head,head_size), dtype=torch.int8 )
-#     key_cache_npu = key_cache.npu()
-
-#     # key = torch.rand((num_tokens, num_head, head_size), dtype=torch.float16)
-#     # while count >0: 
-#     # num_tokens=num_tokens+200*zt_i
-#     slot_list=[]
-#     for i in range(0,num_tokens):
-#         # slot_list.append([6+i])
-#         slot_list.append(2+i)
-#     # slot_list=[29572, 29236, 29406, 29542, 28872, 28023, 29098, 29184]
-#     # slot_list=[29572 ,29236 , 29184,  29406, 29542, 28872, 28023, 29098]
-#     # slot_list.append(-1)
-#     # slot_list.append(-1)
-#     #assert num_tokens==len(slot_list)
-    
-#     slot_list_np= np.array(slot_list)
-#     slot_mapping_npu = torch.from_numpy(slot_list_np).to(torch.int32).npu()
-#     #slot_mapping_cpu = slot_mapping_npu.to("cpu",non_blocking=True)
-#     # num_draft_tensor = slot_mapping_npu.to("cpu", non_blocking=True)
-#     slot_mapping_cpu = torch.empty_like(slot_mapping_npu, device="cpu").pin_memory()
-#     slot_mapping_cpu.copy_(slot_mapping_npu, non_blocking=True)
-
-#     # key = torch.rand((num_tokens, num_head,head_size), dtype=torch.float16)
-#     key = torch.randint(low=0,high=128,size=(num_tokens,head_size), dtype=torch.int8 )
-#     key_npu = key.npu()
-#     # key_expect = cal_slot(key_npu, key_cache_npu, slot_list_np, block_size)
-
-
-
-#     time.sleep(0.1)
-        
-#     # print(f"    test_myops            ")
-#     # for zt_i in range(100):
-#     # 调用你的算子
-
-
-#     slot_mapping_list = slot_mapping_cpu.tolist() 
-#     warm_up=0
-#     for _ in range(warm_up):
-
-#         group_len,group_key_idx,group_key_cache_idx=torch.ops._C_ascend.store_kv_block_pre( slot_mapping_npu, slot_mapping_list, block_size)
-#         torch.ops._C_ascend.store_kv_block(key_npu, key_cache_npu, group_len, group_key_idx, group_key_cache_idx, block_size)
-#     N = 100
-#     #group_len,group_key_idx,group_key_cache_idx=torch.ops._C_ascend.cache_by_group_pre( slot_mapping_npu, slot_mapping_list, block_size)
-#     start = time.perf_counter()
-#     for _ in range(N):
-
-#         # group_list_np= np.array(group_list)
-#         # prof.start()
-#         group_len,group_key_idx,group_key_cache_idx=torch.ops._C_ascend.store_kv_block_pre( slot_mapping_npu, slot_mapping_list, block_size)
-#         torch.ops._C_ascend.store_kv_block(key_npu, key_cache_npu, group_len, group_key_idx, group_key_cache_idx, block_size)
-#         # prof.stop()
-#     end = time.perf_counter()
-#     avg_ms = (end - start) / N * 1000
-#     print(f"python 耗时: {avg_ms:.4f} ms")
-
-#     # torch.equal(key_expect, key_cache_npu)   
-#     # torch.testing.assert_close(key_expect, key_cache_npu, atol=0.001, rtol=0.1)
-#     #torch.testing.assert_close(key_expect, key_cache_npu, atol=0.001, rtol=0.1 )
-#     # # 转 MB 方便看
-#     # mb_size = byte_size / 1024 / 1024
-#     # print(f"    groupInfo Tensor 显存大小：{mb_size:.4f} MB")
-#     # # del groupInfo
-#     # # torch.npu.empty_cache()
-#     # # # 同步，保证算子执行完
-#     # torch.npu.synchronize()
-
-#     # # 每 100 次打印一次显存
-#     # if zt_i % 10 == 0:
-#     #     allocated = torch.npu.memory_allocated() / 1024 / 1024
-#     #     reserved = torch.npu.memory_reserved() / 1024 / 1024
-#     #         print(f"【第 {zt_i} 次】 allocated: {allocated:.4f} MB | reserved: {reserved:.4f} MB")
-        
-#     gc.collect()
-#     torch.npu.empty_cache()
-#     torch.npu.reset_peak_memory_stats()
-#         # count=count-1
